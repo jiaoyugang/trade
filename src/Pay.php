a3a3360c7dc4512f5c85940057147ef5c741826d
gang <1355528968@qq.com>
2020-08-20 22:21:21 +0800

    微信支付

--- src/Pay.php
+++ src/Pay.php
@@ -5,22 +5,6 @@ use Kongflower\Pay\Exception\WxPayException;
 
 class Pay
 {
-
-    /**
-     * 支付配置参数初始化
-     *
-     * @var array
-     */
-    protected $config;
-    
-    /**
-     * 初始化支付配置信息
-     */
-    public function __construct($config)
-    {   
-        $this->config = $config;
-    }
-
     /**
      * @param [type] 支付对象
      * @param [type] 参数
@@ -32,6 +16,7 @@ class Pay
         if(!class_exists($gateway)){
             throw new WxPayException('仅支持微信(Weichat)或支付宝支付(Alipay)');
         }
-        var_dump($method, $arguments[0]);
+        return (new $gateway());
+        // var_dump($gateway, $arguments[0]);
     }
 }
--- test/pay.php
+++ test/pay.php
@@ -4,26 +4,41 @@ use Kongflower\Pay\Pay;
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-/**
- * 支付公共参数初始化
- */
-$pay = new Pay([
-    'appid' => 'wxb829f31ecd32bdf5',
-    'mch_id' => '10037582',
-    'key' => '3phqOIhrvQjCAwdEqpjosP4913ZpEskN',
-]);
+// /**
+//  * 支付公共参数初始化
+//  */
+// $pay = new Pay([
+//     'appid' => 'wxb829f31ecd32bdf5',
+//     'mch_id' => '10037582',
+//     'key' => '3phqOIhrvQjCAwdEqpjosP4913ZpEskN',
+// ]);
 
-$app = [
-        'body' => '测试-充值测试',
-        'notify_url' => 'https://pay.skinrun.cn/pay_callback',
-        'out_trade_no' => '202054541484',
-        'total_fee' => 1,
-        'openid' => 'oTrN9t1gi91ER6k98_Wv7E1W1jeA', //JSAPI支付必传参数
-    ];
-$pay::Weichat([
-    'body' => '测试-充值测试',
-    'notify_url' => 'https://pay.skinrun.cn/pay_callback',
-    'out_trade_no' => '202054541484',
-    'total_fee' => 1,
-    'openid' => 'oTrN9t1gi91ER6k98_Wv7E1W1jeA', //JSAPI支付必传参数
-    ],['ceshi']);
+// $app = [
+//         'body' => '测试-充值测试',
+//         'notify_url' => 'https://pay.skinrun.cn/pay_callback',
+//         'out_trade_no' => '202054541484',
+//         'total_fee' => 1,
+//         'openid' => 'oTrN9t1gi91ER6k98_Wv7E1W1jeA', //JSAPI支付必传参数
+//     ];
+// $pay::Weichat([
+//     'body' => '测试-充值测试',
+//     'notify_url' => 'https://pay.skinrun.cn/pay_callback',
+//     'out_trade_no' => '202054541484',
+//     'total_fee' => 1,
+//     'openid' => 'oTrN9t1gi91ER6k98_Wv7E1W1jeA', //JSAPI支付必传参数
+//     ],['ceshi']);
+
+$config = [
+            'appid' => 'wxb829f31ecd32bdf5',
+            'mch_id' => '10037582',
+            'key' => '3phqOIhrvQjCAwdEqpjosP4913ZpEskN',
+         ];
+$pay  = Pay::Weichat($config)->pay('app',['title' => 'APP支付']);
+
+$order = [
+            'body' => '测试-充值测试',
+            'notify_url' => 'https://pay.skinrun.cn/pay_callback',
+            'out_trade_no' => '202054541484',
+            'total_fee' => 1,
+            'openid' => 'oTrN9t1gi91ER6k98_Wv7E1W1jeA', //JSAPI支付必传参数
+        ];
